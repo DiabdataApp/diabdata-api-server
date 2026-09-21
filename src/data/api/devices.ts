bbdcc0810416a6834cab7df2eed500255506e345
Florian Cossu <florian.cossu@gmail.com>
2026-09-22 00:47:46 +0200

    [FEAT] Added CSVws + upcoming api - Added CSV storing registries that were previously stored in App assets and made app rebuild and re release on update. - Created CSV to Map conversion on app startup - Removed deprecated NGINX conf - Cleaned up legacy docker compose - Upgraded dependencies - Updated docker image tags during workflow build

---
+++ src/data/api/devices.ts
@@ -0,0 +1,34 @@
+import type { MedicalDevice } from "../../types/api/medical_devices";
+import type { DeviceType } from "../../types/api/medical_devices/device_type";
+
+export async function getMedicalDeviceRegistry(): Promise<
+  Map<string, MedicalDevice>
+> {
+  const content = await Bun.file("assets/medical_devices.csv").text();
+  const map = new Map<string, MedicalDevice>();
+
+  const lines = content.trim().split("\n");
+  const rows = lines.slice(1);
+
+  for (let medicalDevice of rows) {
+    let device = medicalDevice.split(",");
+
+    if (device.length == 5) {
+      const GTIN = device[0]?.trim();
+
+      if (!GTIN) continue;
+
+      let medicalDeviceObject: MedicalDevice = {
+        gtin: GTIN,
+        manufacturer: device[1]?.trim() ?? "",
+        deviceType: (device[2]?.trim() ?? "UNKNOWN") as DeviceType,
+        fullName: device[3]?.trim() ?? "",
+        daysLifespan: parseInt(device[4]?.trim() ?? "0"),
+      };
+
+      map.set(GTIN, medicalDeviceObject);
+    }
+  }
+
+  return map;
+}
---
+++ src/data/api/medications.ts
@@ -0,0 +1,33 @@
+import type { Medication } from "../../types/api/medication";
+import type { MedicationType } from "../../types/api/medication/medication_type";
+
+export async function getMedicationRegistry(): Promise<
+  Map<string, Medication>
+> {
+  const content = await Bun.file("assets/medication_data.csv").text();
+  const map = new Map<string, Medication>();
+
+  const lines = content.trim().split("\n");
+  const rows = lines.slice(1);
+
+  for (let medicationEntry of rows) {
+    let medication = medicationEntry.split(",");
+
+    if (medication.length === 4) {
+      const GTIN = medication[0]?.trim();
+
+      if (!GTIN) continue;
+
+      let medicationObject: Medication = {
+        gtin: GTIN,
+        insulin: medication[1]?.trim() ?? "",
+        treatmentType: (medication[2]?.trim() ?? "UNKNOWN") as MedicationType,
+        fullName: medication[3]?.trim() ?? "",
+      };
+
+      map.set(GTIN, medicationObject);
+    }
+  }
+
+  return map;
+}
